c98ee0831de56300f7ec70332a7dc7269d267586
Juan Barbera Filho <jbarberafilho@outlook.com>
2022-06-07 18:48:42 +0200

    client built

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ mongoose.connect(uri)
 ;
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
 }
 
